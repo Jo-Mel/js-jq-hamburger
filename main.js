c64605b982c19f6dc64d007ae60735aca7fabfdb
Jona M <jonameleqi@gmail.com>
2020-07-22 17:18:44 +0200

    meglio?

--- main.js
+++ main.js
@@ -2,20 +2,20 @@
 
 //** Versione show/hide **/
 
-$('i.fa-bars').click(function() {
+$('a i').click(function() {
     $('.hamburger-menu').show();
 });
 
-$('i.fa-times').click(function() {
+$('.close').click(function() {
     $('.hamburger-menu').hide();
 });
 
 //** Versione fadeIn/fadeOut **//
 
-// $('i.fa-bars').click(function(){
+// $('a i').click(function(){
 //     $('.hamburger-menu').fadeIn('fast');
 // });
 
-// $('i.fa-times').click(function(){
+// $('.close').click(function(){
 //     $('.hamburger-menu').fadeOut('fast');
 // });
